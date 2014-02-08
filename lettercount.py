35250a38b6495632dceaccc2efc6ed3d852fe0fe
Lauren Budorick <Lauren@lauren-budoricks-macbook-pro.local>
2014-02-07 17:02:17 -0800

    consolidated loops for efficiency

--- lettercount.py
+++ lettercount.py
@@ -21,32 +21,15 @@ f.close()
 
 lowerfile = filetext.lower()
 
-# divide into individual character strings
-chars = []
-for char in lowerfile:
-	chars.append(char)
-	
-# convert all to ords
-ords = []
-for char in chars:
-	ords.append(ord(char))
-
-# only want letters, strip the rest
-letterords = []
-for theord in ords:
-	if 97 <= theord <= 122:
-		letterords.append(theord)
-
-# make these nicer numbers
-letterords[:] = [x - 97 for x in letterords]
-
-# initialize empty counter list
+# initialize letter counter list
 lettercounts = [0] * 26
 
-# count letters!
-for i in range(len(letterords)):
-	lettercounts[letterords[i]] += 1
-
-# print letter counts so that we can pipe this list to spark
+# loop through file, find letters, iterate on relevant counter
+for char in lowerfile:
+	char = ord(char)
+	if 97 <= char <= 122:
+		char -= 97
+		lettercounts[char] += 1
+		
 for thecount in lettercounts:
     print thecount
